1a29de7443015542f8f24ba9a6bc2142ce2cfbe0
Denver Smith <denverpsmith@gmail.com>
2021-01-06 00:30:00 -0700

    Simplify pathfinding algorithm

--- lib/sternhalma/pathfinding.ex
+++ lib/sternhalma/pathfinding.ex
@@ -6,7 +6,6 @@ defmodule Sternhalma.Pathfinding do
     - The finishing spot is empty and is a direct neighbor of the starting spot.
     - The finishing spot is empty and is reachable via one or more "jump" moves.
 
-  TODO: explain jump moves
   """
 
   alias Sternhalma.{Hex, Cell, Board}
@@ -26,31 +25,83 @@ defmodule Sternhalma.Pathfinding do
         [start, finish]
 
       true ->
-        paths =
-          jump_move(
-            board,
-            start,
-            finish,
-            %{start => :done},
-            [{nil, start}]
-          )
-
-        backtrack(paths, finish, [])
+        path = bfs(board, finish, %{}, %{}, :done, [start])
+        backtrack(path, finish, [])
     end
   end
 
-  @type next_location :: nil | :done | Cell.t()
+  @type path :: %{Cell.t() => :done | Cell.t()}
+  @type visited :: %{Hex.t() => true}
 
-  @typedoc """
-  Represents the chain of steps needed to create a path of cells.
-  The keys are always cells and the values can be:
-    - A cell when there is another location to move to
-    - :done when the desired location is reached
-    - nil when the location is invalid
+  @doc """
+  Find the shorted path to the provided target
+  cell, if exists.
+
+  The general algorithm is
+  Breadth First Search. First dequeue a cell
+  for exploration, next enqueue neighbors and
+  repeat.
   """
-  @type path_guide :: %{Cell.t() => next_location()}
+  @spec bfs(Board.t(), Cell.t(), path(), visited(), Cell.t() | :done, list(Cell.t())) :: path()
+  defp bfs(_board, target, path, _visited, parent, [current | _to_be_explored])
+       when current.position == target.position,
+       do: Map.put(path, current, parent)
 
-  @spec backtrack(path_guide(), next_location(), list(Cell.t())) :: list(Cell.t())
+  defp bfs(_board, _target, path, _visited, _parent, []), do: path
+
+  defp bfs(board, target, path, visited, parent, [current | to_be_explored]) do
+    path =
+      if Map.get(visited, current.position) == nil do
+        Map.put(path, current, parent)
+      else
+        path
+      end
+
+    visited = Map.put(visited, current.position, true)
+
+    neighbor_cells =
+      current.position
+      |> jumpable_neighbors(board)
+      |> remove_invalid_cells(board)
+      |> remove_visited_cells(visited)
+
+    bfs(board, target, path, visited, current, to_be_explored ++ neighbor_cells)
+  end
+
+  @doc """
+  Return all the neighbors from a given position
+  on a board. It's different from Hex.neighbors
+  because this counts neighbors as either:
+
+  - Empty cells directly next to the given position
+  - Empty cells that are one jump away from the given
+    position from a single direction
+
+  This function is helpful because from this module's
+  perspective the only neighbors that are important
+  are those that are reachable.
+
+  See Hex.neighbors/1 or Hex.neighbor/2 for finding
+  neighbors without these rules.
+  """
+  @spec jumpable_neighbors(Hex.t(), Board.t()) :: list(Cell.t())
+  def jumpable_neighbors(position, board) do
+    position
+    |> Hex.neighbors()
+    |> Enum.map(fn {direction, position} ->
+      case Enum.find(board, &(&1.position == position and &1.marble != nil)) do
+        nil ->
+          nil
+
+        cell ->
+          pos = Hex.neighbor(cell.position, direction)
+          Enum.find(board, &(&1.position == pos and &1.marble == nil))
+      end
+    end)
+    |> Enum.filter(& &1)
+  end
+
+  # @spec backtrack(path_guide(), next_location(), list(Cell.t())) :: list(Cell.t())
   defp backtrack(_paths, nil, _path), do: []
   defp backtrack(_paths, :done, path), do: path
 
@@ -59,96 +110,20 @@ defmodule Sternhalma.Pathfinding do
     backtrack(paths, current, [finish | path])
   end
 
-  @type jump_direction :: nil | Hex.direction()
+  # TODO: might not need this
 
-  @spec jump_move(Board.t(), Cell.t(), Cell.t(), path_guide(), list({jump_direction(), Cell.t()})) ::
-          path_guide()
-  defp jump_move(_board, _start, _finish, came_from, []), do: came_from
-
-  defp jump_move(_board, _start, finish, came_from, [{_direction, current} | _cells])
-       when finish.position == current.position do
-    came_from
-  end
-
-  defp jump_move(board, start, finish, came_from, [{direction, current} | cells]) do
-    neighboring_cells =
-      current
-      |> neighbors(direction)
-      |> remove_invalid_cells(board)
-      |> convert_hex_positions_to_cells(board)
-      |> filter_occupied_cells(direction)
-      |> remove_visited_cells(came_from)
-
-    next_cells =
-      neighboring_cells
-      |> Enum.filter(fn {_, c} -> Enum.find(cells, fn {_, cc} -> cc == c end) == nil end)
-      |> Kernel.++(cells)
-
-    came_from =
-      update_came_from(
-        came_from,
-        current,
-        Enum.map(next_cells, fn {_direction, cell} -> cell end)
-      )
-
-    jump_move(board, start, finish, came_from, next_cells)
-  end
-
-  @spec update_came_from(path_guide(), Cell.t(), list(Cell.t())) :: path_guide()
-  defp update_came_from(came_from, current, next_cells) do
-    with [next_neighbor | _] <- next_cells do
-      Map.put(came_from, next_neighbor, current)
-    else
-      _ -> came_from
-    end
-  end
-
-  @spec convert_hex_positions_to_cells(list({jump_direction(), Hex.t()}), Board.t()) ::
-          list({jump_direction(), Cell.t()})
-  defp convert_hex_positions_to_cells(neighbors, board) do
-    Enum.reduce(neighbors, [], fn {direction, position}, acc ->
-      case Enum.find(board, &(&1.position == position)) do
-        nil ->
-          acc
-
-        cell ->
-          [{direction, cell} | acc]
-      end
-    end)
-  end
-
-  @spec neighbors(Cell.t(), jump_direction()) :: list({jump_direction(), Hex.t()})
-  defp neighbors(cell, nil), do: Hex.neighbors(cell.position)
-  defp neighbors(cell, direction), do: [{nil, Hex.neighbor(cell.position, direction)}]
-
-  @spec remove_invalid_cells(list({jump_direction(), Hex.t()}), Board.t()) ::
-          list({jump_direction(), Hex.t()})
+  @spec remove_invalid_cells(list(Cell.t()), Board.t()) :: list(Cell.t())
   defp remove_invalid_cells(neighbors, board) do
     neighbors
-    |> Enum.filter(fn {_direction, neighbor} ->
-      Enum.any?(board, fn cell -> neighbor == cell.position end)
+    |> Enum.filter(fn neighbor_cell ->
+      Enum.any?(board, fn cell -> neighbor_cell.position == cell.position end)
     end)
   end
 
-  @spec remove_visited_cells(list({jump_direction(), Cell.t()}), path_guide()) ::
-          list({jump_direction(), Cell.t()})
-  defp remove_visited_cells(cells, came_from) do
-    cells
-    |> Enum.reject(fn {_direction, cell} -> Map.get(came_from, cell) end)
-  end
-
-  @spec filter_occupied_cells(list({jump_direction(), Cell.t()}), jump_direction()) ::
-          list({jump_direction(), Cell.t()})
-  defp filter_occupied_cells(cells, nil) do
-    Enum.reject(cells, fn {_direction, cell} ->
-      cell.marble == nil
-    end)
-  end
-
-  defp filter_occupied_cells(cells, _direction) do
-    Enum.filter(cells, fn {_direction, cell} ->
-      cell.marble == nil
-    end)
+  @spec remove_visited_cells(list(Cell.t()), visited()) :: list(Cell.t())
+  defp remove_visited_cells(neighbors, visited) do
+    neighbors
+    |> Enum.reject(fn cell -> Map.get(visited, cell.position) end)
   end
 
   @spec jump_needed?(Cell.t(), Cell.t()) :: boolean()
